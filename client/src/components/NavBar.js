325acc1e799bd30fa365b8440484d2137fb04b6f
nkchambers <nkchames@gmail.com>
2023-12-28 14:47:51 -0800

    initial commit

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -40,14 +40,14 @@ export const NavBar = () => {
         <Router>
             <Navbar expand="md" className={scrolled ? 'scrolled' : ''}>
                 <Container>
-                    <Navbar.Brand href="#home" className='p-1'>
+                    <Navbar.Brand href="#home" className='m-1'>
                         <img src={NC_logo} alt='Logo' className='ncLogo' />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className='navbar-toggler-icon'></span>
                     </Navbar.Toggle>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="me-auto p-1">
                             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() =>onUpdateActiveLink('home')}>Home</Nav.Link>
                             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() =>onUpdateActiveLink('skills')}>Skills</Nav.Link>
                             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() =>onUpdateActiveLink('projects')}>Projects</Nav.Link>
