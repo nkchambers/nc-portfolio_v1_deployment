2de8dc9cf3187abd817680ba3e52962f097f236f
nkchambers <nkchames@gmail.com>
2024-01-16 09:59:11 -0800

    modified footer linkedln url

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,7 +17,7 @@ export const Footer = () => {
                     </Col>
                     <Col size={12} sm={6} className="text-center text-sm-end pt-4">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/nick-chambers-0a391241/"><img src={navIcon1} alt="Linkedln Icon" /></a>
+                            <a href="https://www.linkedin.com/in/nicholas-chambers-0a391241/"><img src={navIcon1} alt="Linkedln Icon" /></a>
                             <a href="https://www.facebook.com/nick.chambers.370"><img src={navIcon2} alt="Facebook Icon" /></a>
                             <a href="https://www.instagram.com/chambers9565/"><img src={navIcon3} alt="Instagram Icon" /></a>
                         </div>
